4de3ecc4b7752e2d4dab430b81445a2e534a3f9d
crileyy <criley0007@gmail.com>
2025-01-29 17:50:32 -0500

    Create progressbar component

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,63 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  let ProgressBarWrapper;
+  let ProgressBarValue;
+  if (size === 'large') {
+    ProgressBarWrapper = LargeProgressBarWrapper;
+    ProgressBarValue = LargeProgressBarValue;
+  } else if (size === 'medium') {
+    ProgressBarWrapper = MediumProgressBarWrapper;
+    ProgressBarValue = MediumProgressBarValue;
+  } else if (size === 'small') {
+    ProgressBarWrapper = SmallProgressBarWrapper;
+    ProgressBarValue = SmallProgressBarValue;
+  } else {
+    throw new Error('invalid size');
+  }
+  return (
+    <ProgressBarWrapper
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemax={100}
+      aria-valuemin={0}
+    >
+      <ProgressBarValue value={value} style={{ '--progress-width': value + '%' }} />
+    </ProgressBarWrapper>);
 };
+
+const SmallProgressBarValue = styled.div`
+  width: var(--progress-width);
+  height: 8px;
+  background-color: ${COLORS.primary};
+  border-radius: ${p => '4px ' + (4 * p.value/100) + 'px ' + (4 * p.value/100) + 'px ' + '4px'};
+`
+
+const SmallProgressBarWrapper = styled.div`
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 4px;
+  height: 8px;
+  width: 370px;
+  max-width: 370px;
+`;
+
+const MediumProgressBarValue = styled(SmallProgressBarValue)`
+  height: 12px;
+`
+
+const MediumProgressBarWrapper = styled(SmallProgressBarWrapper)`
+  height: 12px;
+`;
+
+const LargeProgressBarValue = styled(SmallProgressBarValue)`
+  height: 16px;
+`
+
+const LargeProgressBarWrapper = styled(SmallProgressBarWrapper)`
+  height: 24px;
+  border-radius: 8px;
+  padding: 4px;
+`;
 
 export default ProgressBar;
