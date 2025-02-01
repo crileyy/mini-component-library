3fedb48065d5548575342bc640cc844037195e8d
crileyy <criley0007@gmail.com>
2025-02-01 17:26:13 -0500

    IconInput component

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,21 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const sizes = {
+  small: {
+    '--icon-size': '16px',
+    'font-size': '14px',
+    '--icon-bottom': '4px',
+    '--wrapper-height': '24px',
+  },
+  large: {
+    '--icon-size': '24px',
+    'font-size': '18px',
+    '--icon-bottom': '6px',
+    '--wrapper-height': '36px',
+  },
+}
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +28,62 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const sizeStyle = sizes[size];
+
+  return (
+    <>
+      <Wrapper style={{ '--width': width + 'px', ...sizeStyle }}>
+        <IconWrapper style={sizeStyle}>
+          <Icon id={icon} strokeWidth={1} size={size === 'small' ? 16 : 24} />
+        </IconWrapper>
+        <InputWrapper>
+          <StyledInput placeholder={placeholder} style={sizeStyle} />
+        </InputWrapper>
+      </Wrapper>
+    </>
+  );
 };
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  width: var(--icon-size);
+  height: var(--icon-size);
+`;
+
+const InputWrapper = styled.div`
+  padding-left: 24px;
+  padding-top: 4px;
+`;
+
+const StyledInput = styled.input`
+  border: none;
+  width: 100%;
+  font-weight: 700;
+  color: ${COLORS.gray700};
+  
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const Wrapper = styled.div`
+  width: var(--width);
+  border-bottom: 1px solid ${COLORS.black};
+  position: relative;
+  height: var(--wrapper-height);
+  
+  &:focus-within {
+    outline: 1px dotted #212121;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+`;
 
 export default IconInput;
